355538c825249617bff5f23ab692bc2c8c7612a8
jazeps <jazepsrutkis93@gmail.com>
2021-01-30 18:57:21 +0100

    *Added deadly stone trap

---
+++ Assets/Scripts/GameSystem/Components/DeadlyRockSwitch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameSystem.Dto;
+using GameSystem;
+
+public class DeadlyRockSwitch : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private bool destroyOnCollide = false;
+
+
+    private void Start()
+    {
+        rb.isKinematic = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.IsPlayerCollision()) return;
+
+        rb.isKinematic = false;
+        if (destroyOnCollide)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!other.IsPlayerCollision()) return;
+        rb.isKinematic = false;
+        if (destroyOnCollide)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Interactables/DisableDeathOnCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableDeathOnCollision : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            GetComponent<GameSystem.Components.DeathCollider>().enabled = false;
+        }
+    }
+}
